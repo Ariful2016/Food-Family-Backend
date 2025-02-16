585352a0428d105aeef0381be6399f39d558233d
Ariful <arifewucse2016@gmail.com>
2025-02-16 17:19:24 +0600

    generate and send otp

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ const AddressRoute = require("./routes/address");
 const CartRoute = require("./routes/cart");
 const OrderRoute = require("./routes/order");
 
+//const sendEmail = require('./utils/smtp_function');
+//const generateOtp = require('./utils/otp_generator');
+
 
 dotenv.config();
 
@@ -19,7 +22,9 @@ mongoose.connect(process.env.MONGOURL)
 .then(() => console.log("Foodl Family Database Connected"))
 .catch((err) => console.log(err));
 
-
+//const otp = generateOtp();
+//console.log(otp);
+//sendEmail('arifewucse2016@gmail.com', otp);
 
 app.use(express.json());                 
 app.use(express.urlencoded({extended: true}));
